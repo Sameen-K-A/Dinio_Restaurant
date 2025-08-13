460944e4054d5787dabcc8af8c4a3dbcbf3cf77b
Sameen-K-A <sameensameen60@gmail.com>
2025-08-13 19:52:25 +0530

    change position to bottom

--- src/components/shared/WaiterCallPopup.tsx
+++ src/components/shared/WaiterCallPopup.tsx
@@ -8,7 +8,7 @@ interface WaiterCallPopupProps {
 
 export default function WaiterCallPopup({ tableNumber, show }: WaiterCallPopupProps) {
   return (
-    <div className={cn("fixed top-4 left-1/2 -translate-x-1/2 z-50 transition-transform duration-300 ease-in-out", show ? "translate-y-0 opacity-100" : "-translate-y-20 opacity-0")}>
+    <div className={cn("fixed bottom-4 left-1/2 -translate-x-1/2 z-50 transition-transform duration-300 ease-in-out", show ? "translate-y-0 opacity-100" : "-translate-y-20 opacity-0")}>
       <div className="flex items-center justify-between gap-4 bg-[#ffffff] dark:bg-[#232323] border rounded-full p-2 w-full shadow-lg">
         <div className="flex items-center justify-center w-15 h-15 rounded-full bg-green-500">
           <PhoneCall size={22} className="animate-shake text-white" />
